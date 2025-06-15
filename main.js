d988421cc7359ac70b5b1d95cdac404657dc7f55
Rupesh Kumar Thapa <081bct064@ioepc.edu.np>
2025-06-15 20:58:09 +0545

    Initial commit

--- main.js
+++ main.js
@@ -3,14 +3,16 @@
 //   h2s[i].style.color = "green";
 // }
 
-let hamburger = document.querySelector(".hamburger");
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector(".close-btn");
+document.addEventListener("DOMContentLoaded", function () {
+  let hamburger = document.querySelector(".hamburger");
+  let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector(".close-btn");
 
-hamburger.addEventListener("click", () => {
-  sidebar.classList.add("active");
-});
+  hamburger.addEventListener("click", () => {
+    sidebar.classList.add("active");
+  });
 
-closeBtn.addEventListener("click", () => {
-  sidebar.classList.remove("active");
+  closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+  });
 });
